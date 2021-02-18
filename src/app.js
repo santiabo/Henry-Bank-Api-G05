23b09a15f99bee00e27a8e14a6f13cbbdd274e41
Santiago Aguirre <santiabo@gmail.com>
2021-02-18 08:32:58 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,12 +16,12 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-/* server.use(
+ server.use(
 	cors({
-		origin      : 'http://localhost:19006',
+		origin      : 'https://bank-tree.herokuapp.com',
 		credentials : true
 	})
-); */
+); 
 server.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
 	res.header('Access-Control-Allow-Credentials', 'true');
