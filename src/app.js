eaa9f397ed80c1fa3e64effcad34badc7390146c
Santiago Aguirre <santiabo@gmail.com>
2021-02-18 12:46:22 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ server.use(function(req, res, next) {
 res.header('Access-Control-Allow-Credentials', true);
 res.header('Access-Control-Allow-Origin', req.headers.origin);
 res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Length, Content-Type, Accept'); 
+	
 if ('OPTIONS' == req.method) {
      res.send(200);
  } else {
